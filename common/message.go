e23236fd21e9e68a6714c2be7d5d4cf14cc2ac52
Andrew Zak <andrew@linux.com>
2019-05-09 20:51:22 +0100

    added the ability to get data as raw bytes

--- common/message.go
+++ common/message.go
@@ -1,6 +1,22 @@
 package common
 
+import (
+	"encoding/json"
+	"bytes"
+	"encoding/gob"
+)
+
 type Message struct {
 	EventName string      `json:"eventName"`
 	Data      interface{} `json:"data"`
+}
+
+func (m *Message) DataAsRaw() json.RawMessage {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(m.Data)
+	if err != nil {
+		return nil
+	}
+	return buf.Bytes()
 }
