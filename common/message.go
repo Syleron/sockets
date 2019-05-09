93249cbf74f8a5693ef43f43b7ce8bc03a4452ac
Andrew Zak <andrew@linux.com>
2019-05-09 21:54:09 +0100

    reversed the removal of the response struct

--- common/message.go
+++ common/message.go
@@ -2,21 +2,15 @@ package common
 
 import (
 	"encoding/json"
-	"bytes"
-	"encoding/gob"
 )
 
-type Message struct {
-	EventName string      `json:"eventName"`
-	Data      interface{} `json:"data"`
+type Response struct {
+	EventName string `json:"eventName"`
+	Data interface{} `json:"data"`
 }
 
-func (m *Message) DataAsRaw() json.RawMessage {
-	var buf bytes.Buffer
-	enc := gob.NewEncoder(&buf)
-	err := enc.Encode(m.Data)
-	if err != nil {
-		return nil
-	}
-	return buf.Bytes()
+type Message struct {
+	EventName string          `json:"eventName"`
+	Data      json.RawMessage `json:"data"`
 }
+
